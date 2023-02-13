ccaba949d6635bc6ed11e97853a7e974672fca46
daniellaszlo123 <daniellaszlo002@gmail.com>
2023-02-13 09:55:18 +0100

    konstruktor potlas

--- src/modell/Auto.java
+++ src/modell/Auto.java
@@ -4,6 +4,11 @@ package modell;
 
 public class Auto extends Jarmu{
     private boolean defekt;
+
+    public Auto(boolean defekt) {
+        super(false, true, false);
+        this.defekt = defekt;
+    }
     
     public void kereketCserel(){}
     @Override
--- src/modell/Jarmu.java
+++ src/modell/Jarmu.java
@@ -5,6 +5,12 @@ public abstract class Jarmu {
     private boolean beinditva;
     private boolean uzemanyag;
     private boolean megerkezett;
+
+    public Jarmu(boolean beinditva, boolean uzemanyag, boolean megerkezett) {
+        this.beinditva = beinditva;
+        this.uzemanyag = uzemanyag;
+        this.megerkezett = megerkezett;
+    }
     
     public void bindit(){}
     public void leallit(){}
